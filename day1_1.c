8f178224f1468264e5146c8abe4c2aecacca867a
Chetanmurchite <chetanmurchitte@gmail.com>
2024-06-28 16:41:21 +0530

    day1

---
+++ day1_1.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#define SIZE 9
+
+int linear_search(int arr[],int key);
+int comparisons;
+
+
+int main()
+{
+    int arr[SIZE] = {33,55,88,77,44,11,66,22,99};
+    int key;
+
+    printf("Enter the key to search: ");
+    scanf("%d",&key);
+
+    int index = linear_search(arr,key);
+
+    if(index == -1)
+        printf("Key not Found !\n");
+    else
+        printf("Key is found at index %d\n",index);
+
+    printf("Comparisons = %d\n",comparisons);  
+    return 0;      
+}
+
+int linear_search(int arr[], int key)
+{
+    for(int i = 0; i<SIZE; i++)
+    {
+        comparisons++;
+        if(key == arr[i])
+        {
+            return i;
+        }
+    }
+    return -1;   
+}
+
+
+
+
+
